1206e6b0159d147805e192d424107009a323584e
ChezyName <mineboy2468@gmail.com>
2023-08-26 22:05:24 -0500

    Added After Images + Dash VFX

--- Source/KatanaHeroReborn/KatanaHeroRebornCharacter.cpp
+++ Source/KatanaHeroReborn/KatanaHeroRebornCharacter.cpp
@@ -1,11 +1,74 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "KatanaHeroRebornCharacter.h"
+
+#include "DrawDebugHelpers.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void AKatanaHeroRebornCharacter::DisplayAfterImages()
+{
+	AActor* VFX = UGameplayStatics::BeginDeferredActorSpawnFromClass(this,AfterImages,FTransform(FRotator::ZeroRotator,GetActorLocation()),ESpawnActorCollisionHandlingMethod::AlwaysSpawn,this);
+	UGameplayStatics::FinishSpawningActor(VFX, FTransform(FRotator::ZeroRotator,GetActorLocation()));
+}
+
+FVector AKatanaHeroRebornCharacter::GetMouseLocation()
+{
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (PC)
+	{
+		FVector WorldLoc, WorldDir;
+		if (PC->DeprojectMousePositionToWorld(WorldLoc, WorldDir))
+		{
+			return WorldLoc;
+		}
+	}
+	return FVector::ZeroVector;
+}
+
+void AKatanaHeroRebornCharacter::LookAtMouse()
+{
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (!PC)
+		return;
+
+	FVector MouseWorldPos, MouseWorldDir;
+	if (!PC->DeprojectMousePositionToWorld(MouseWorldPos, MouseWorldDir))
+		return;
+
+	FVector CharacterLocation = GetActorLocation();
+	FVector DirectionToMouse = MouseWorldPos - CharacterLocation;
+	DirectionToMouse.Z = 0.0f; // Ignore the Z component to stay in the 2D plane
+	DirectionToMouse.Normalize();
+
+	FRotator LookAtRotation = DirectionToMouse.Rotation();
+	LookAtRotation.Pitch = 0.0f; // Set pitch to zero to keep character upright
+	SetActorRotation(LookAtRotation);
+
+	GetCharacterMovement()->bOrientRotationToMovement = false; // Face in the direction we are moving..
+
+	DrawDebugSphere(GetWorld(), MouseWorldPos, 5, 8, FColor::Green, false, 60, 0, 1);
+	DrawDebugLine(GetWorld(), CharacterLocation, MouseWorldPos, FColor::Red, false, 60, 0, 1);
+}
+
+void AKatanaHeroRebornCharacter::StopLookingAtMouse()
+{
+	SetActorRotation(FRotator::ZeroRotator);
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Face in the direction we are moving..
+}
+
+void AKatanaHeroRebornCharacter::Dash()
+{
+	if(DashCD > 0) return;
+	//const FVector MoveDir = FVector(0,-GetInputAxisValue("MoveRight"),GetInputAxisValue("LookUp"));
+	LaunchCharacter(GetActorForwardVector() * 3500,true,true);
+	DisplayAfterImages();
+	DashCD = 3;
+}
 
 AKatanaHeroRebornCharacter::AKatanaHeroRebornCharacter()
 {
@@ -43,6 +106,9 @@ AKatanaHeroRebornCharacter::AKatanaHeroRebornCharacter()
 
 	Katana = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("KatanaModel"));
 	Katana -> SetupAttachment(GetMesh(),"RHand");
+
+	KatanaHitbox = CreateDefaultSubobject<UBoxComponent>(TEXT("KatanaHitbox"));
+	KatanaHitbox -> SetupAttachment(RootComponent);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -51,19 +117,75 @@ AKatanaHeroRebornCharacter::AKatanaHeroRebornCharacter()
 void AKatanaHeroRebornCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
 {
 	// set up gameplay key bindings
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this,  &AKatanaHeroRebornCharacter::StartJump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 	PlayerInputComponent->BindAxis("MoveRight", this, &AKatanaHeroRebornCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("LookUp", this, &AKatanaHeroRebornCharacter::LookUp);
+
+	PlayerInputComponent->BindAction("Dash", IE_Pressed, this, &AKatanaHeroRebornCharacter::Dash);
+	
+	PlayerInputComponent->BindAction("PrimaryAttack", IE_Pressed, this, &AKatanaHeroRebornCharacter::KatanaAttack);
+}
+
+void AKatanaHeroRebornCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetActorRotation(FRotator(0,-90,0));
 }
 
 void AKatanaHeroRebornCharacter::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 	Katana->SetVisibility(SwordActive);
+	DashCD -= DeltaSeconds;
+}
+
+void AKatanaHeroRebornCharacter::KatanaAttack()
+{
+	if(Attacking) return;
+	SwordActive = true;
+	Attacking = true;
+
+	//Slash Attack HERE
+	PlayAnimMontage(SwordAttachAnimation);
+	StopMovement = true;
+	//LookAtMouse();
+	LaunchCharacter(GetActorForwardVector() * (DashSpeed*100),true,false);
+	DisplayAfterImages();
+
+	FTimerDelegate TimerDelegate;
+	TimerDelegate.BindLambda([&]
+	{
+		StopMovement = false;
+		SwordActive = false;
+		Attacking = false;
+		StopAnimMontage(SwordAttachAnimation);
+		//StopLookingAtMouse();
+	});
+		
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, TimerDelegate, HitboxActiveT, false);
 }
 
 void AKatanaHeroRebornCharacter::MoveRight(float Value)
 {
+	DashDir.Y = -Value;
+	if(StopMovement) return;
 	// add movement in that direction
 	AddMovementInput(FVector(0.f,-1.f,0.f), Value);
+	
+	if(Value == 0) return;
+	if(Value < 0) GetMesh()->SetRelativeScale3D(FVector(-1,1,1));
+	else GetMesh()->SetRelativeScale3D(FVector(1,1,1));
+}
+
+void AKatanaHeroRebornCharacter::LookUp(float Val)
+{
+	DashDir.Y = Val;
+}
+
+void AKatanaHeroRebornCharacter::StartJump()
+{
+	if(!StopMovement) Jump();
 }
--- Source/KatanaHeroReborn/KatanaHeroRebornCharacter.h
+++ Source/KatanaHeroReborn/KatanaHeroRebornCharacter.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
 #include "GameFramework/Character.h"
 #include "KatanaHeroRebornCharacter.generated.h"
 
@@ -22,20 +23,38 @@ class AKatanaHeroRebornCharacter : public ACharacter
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UStaticMeshComponent* Katana;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* KatanaHitbox;
+
 protected:
 
 	/** Called for side to side input */
 	void MoveRight(float Val);
+	void LookUp(float Val);
+	FVector2D DashDir;
+	void StartJump();
+	bool StopMovement = false;
 
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 	// End of APawn interface
 
+	void BeginPlay() override;
 	void Tick(float DeltaSeconds) override;
 
+	//Abilities / Combat
+	void KatanaAttack();
+	bool Attacking = false;
+
+	void Dash();
+
+	void LookAtMouse();
+	void StopLookingAtMouse();
 
 public:
 	AKatanaHeroRebornCharacter();
+
+	FVector GetMouseLocation();
 
 	/** Returns SideViewCameraComponent subobject **/
 	FORCEINLINE class UCameraComponent* GetSideViewCameraComponent() const { return SideViewCameraComponent; }
@@ -44,4 +63,19 @@ public:
 
 	UPROPERTY(BlueprintReadOnly)
 	bool SwordActive = false;
+
+	float DashCD = 0;
+
+	void DisplayAfterImages();
+
+	//Primary Sword Attack
+	UPROPERTY(EditAnywhere,Category="Yasuo")
+	TSubclassOf<AActor> AfterImages;
+	UPROPERTY(EditAnywhere,Category="Yasuo|MainAttack")
+	UAnimMontage* SwordAttachAnimation;
+	UPROPERTY(EditAnywhere,Category="Yasuo|MainAttack")
+	float HitboxActiveT = 0.1;
+	UPROPERTY(EditAnywhere,Category="Yasuo|MainAttack")
+	float DashSpeed = 25;
+	
 };
