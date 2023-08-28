07a0849cbc2850af406972bc586a7801639aa44e
ChezyName <mineboy2468@gmail.com>
2023-08-28 15:36:20 -0500

    Health Upgrades Works

--- Source/KatanaHeroReborn/KatanaHeroRebornGameMode.h
+++ Source/KatanaHeroReborn/KatanaHeroRebornGameMode.h
@@ -3,8 +3,22 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "GameFramework/GameModeBase.h"
 #include "KatanaHeroRebornGameMode.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPlayerUpgrade : public FTableRowBase
+{
+public:
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	int PercentUpgrade = 100;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	int GoldCost = 250;
+
+	
+};
 
 UCLASS(minimalapi)
 class AKatanaHeroRebornGameMode : public AGameModeBase
@@ -13,6 +27,50 @@ class AKatanaHeroRebornGameMode : public AGameModeBase
 
 public:
 	AKatanaHeroRebornGameMode();
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	int HealthUpgrade = 100;
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	int StrengthUpgrade = 100;
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	int SpeedUpgrade = 100;
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	int ManaUpgrade = 100;
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	int APUpgrade = 100;
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	int PotionUpgrade = 100;
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	int GoldUpgrade = 100;
+
+	//===============================================================================================
+	
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	FString HealthName = "1";
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	FString StrengthName = "1";
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	FString SpeedName = "1";
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	FString ManaName = "1";
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	FString APName = "1";
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	FString PotionName = "1";
+
+	UPROPERTY(EditAnywhere,Category="Categories",BlueprintReadWrite,meta=(AllowPrivateAccess=true))
+	FString GoldName = "1";
 };
 
 
